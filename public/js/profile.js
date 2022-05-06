f6287a8248828e18743fc1789c6fc32b93f7c35a
Brant Martin <brantmartin473@gmail.com>
2022-05-05 20:10:16 -0700

    Made it possible to make a post

---
+++ public/js/profile.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#blog-name').value.trim();
+  const post_tags = document.querySelector('#blog-tag').value.trim();
+  const post_body = document.querySelector('#blog-desc').value.trim();
+
+  if (title && post_tags && post_body) {
+    const response = await fetch(`/api/blogs`, {
+      method: 'POST',
+      body: JSON.stringify({ title, post_tags, post_body }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create blog');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete blog');
+    }
+  }
+};
+
+document
+  .querySelector('.new-blog-form')
+  .addEventListener('submit', newFormHandler);
+
+// document
+//   .querySelector('.blog-list')
+//   .addEventListener('click', delButtonHandler);
